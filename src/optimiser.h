b29a1e5ee77cd9a16b1f93c7fb28d2990e120b51
Ayham Saffar <ayham.saffar@gmail.com>
2026-02-27 11:57:11 +0000

    abstract optimser class skeleton implemented

--- src/optimiser.h
+++ src/optimiser.h
@@ -1,5 +1,34 @@
 #ifndef OPTIMISER_H
 #define OPTIMISER_H
 
+#include <vector>
+#include "alloy.h"
+
+namespace optimiser
+{
+    class BaseOptimiser
+    {
+        struct Jacobian{double df1dV{}; double df1dR{}; double df2dV{}; double df2dR{};};
+        
+        private:
+            Jacobian getDerivatives();
+        
+        protected:
+            alloy::Alloy m_A{};
+            double m_V{};
+            double m_R{};
+            double m_f1Error{};
+            double m_f2Error{};
+            virtual double calculatef1() = 0;
+            virtual double calculatef2() = 0;
+
+        public:
+            BaseOptimiser(const alloy::Alloy& A, double V0, double R0)
+                :m_A{A}, m_V{V0}, m_R{R0} {}
+            std::vector<int> step();
+            friend Logger& operator<<(Logger& log, const BaseOptimiser& optimiser);
+    
+    };  
+}
 
 #endif
