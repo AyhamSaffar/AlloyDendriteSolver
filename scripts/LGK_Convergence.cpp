91dcaeaf350b7ef1eadcf3fc54b39b2ab2b03999
Ayham Saffar <ayham.saffar@gmail.com>
2026-03-26 23:19:27 +0000

    fixing missing abs function on convergence checker

--- scripts/LGK_Convergence.cpp
+++ scripts/LGK_Convergence.cpp
@@ -1,5 +1,7 @@
-// Minimal script used to reproduce figure 3 in https://doi.org/10.1016/0025-5416(84)90199-X.
+// Minimal script used track how well the optimiser converges when attempting to reproduce fig.3 in
+// https://doi.org/10.1016/0025-5416(84)90199-X.
 
+#include <vector>
 #include <fstream>
 #include <tuple>
 #include <string>
@@ -10,41 +12,50 @@
 #include "models.h"
 #include "optimiser.h"
 
-struct Result{bool hasDiverged{}; double f1{}; double f2{}; double V{}; double R{};};
+struct Result{std::vector<double> f1s; std::vector<double> f2s;};
 
 Result VRSolver(double dT, double C0, const alloy::Alloy& A)
 {
+    const int nSteps{1000};
     double f1{}, f2{}, V{approx::getTipVelocity(dT, C0, A)}, R{approx::getTipRadius(dT, C0, A)}, dV{}, dR{};
+    std::vector<double> f1s(nSteps), f2s(nSteps);
     diff::Jacobian J{};
 
-    for (int step{0}; step<1000; ++step)
+    for (int step{0}; step<nSteps; ++step)
     {
         std::tie(f1, f2) = models::LGK(V, R, dT, C0, A);
+        f1s[step] = f1;
+        f2s[step] = f2;
         J = diff::calculateGrads<models::LGK>(V, R, dT, C0, A);
         std::tie(dV, dR) = optimisers::newtonRaphson(f1, f2, J);
         if (std::isnan(dV) || std::isnan(dR)) // solver diverges
-            return Result{true};
-        V += dV * 0.1;
-        R += dR * 0.1;
+            break;
+        V += dV*0.1;
+        R += dR*0.1;
         if ((std::abs(f1)<1e-8) && (std::abs(f2)<1e-8)) // solver converged
             break;
     }
     
-    return Result{false, f1, f2, V, R};
+    return Result{f1s, f2s};
 }
 
 int main()
 {
     std::string dataPath{DATA_PATH};
     std::ofstream outf{dataPath + "/data.csv"};
-    outf << "diverged,dT,C0,V,R,f1,f2\n" << std::boolalpha;
+    outf << "dT,C0,f1s,f2s\n" << std::boolalpha;
     for (double dT{0.5}; dT<1.0; dT+=0.4)
     {
-        for(double C0{0.01}; C0<=1.0; C0+=0.01)
+        for(double C0{0.1}; C0<=1.0; C0+=0.1)
         {
             Result result{VRSolver(dT, C0, alloy::SucAce)};
-            outf << result.hasDiverged << ',' << dT << ',' << C0 << ',' << result.V << ',' << result.R << ',' <<
-                result.f1 << ',' << result.f2 << '\n';
+            outf <<  dT << ',' << C0 << ',';
+            for (double f1: result.f1s)
+                outf << f1 << ' ';
+            outf << ',';
+            for (double f2: result.f2s)
+                outf << f2 << ' ';
+            outf << '\n';
         }
     }
     return 0;
