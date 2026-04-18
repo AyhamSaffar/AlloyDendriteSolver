6c0cc6735a265ccad22f5df8dc997850ce74f363
Ayham Saffar <ayham.saffar@gmail.com>
2026-04-19 00:00:02 +0100

    seperated integration and unit tests into different folders

--- tests/integration_tests/LGK_solver_tests.cpp
+++ tests/integration_tests/LGK_solver_tests.cpp
@@ -7,15 +7,15 @@
 #include "differentials.h"
 #include "models.h"
 #include "optimiser.h"
+#include <numbers>
 
-
-// returns iteratively calculated V and R
-std::tuple<double, double> LGKSolver(double dT, double C0, const alloy::Alloy& A)
+// returns iteratively calculated V, R, and f (abs(f1) + abs(f2))
+std::tuple<double, double, double> LGKSolver(double dT, double C0, const alloy::Alloy& A)
 {
     double f1{}, f2{}, dV{}, dR{}, V{approx::getTipVelocity(dT, C0, A)}, R{approx::getTipRadius(dT, C0, A)};
     diff::Jacobian J{};
 
-    for (int step{0}; step<100; ++step)
+    for (int step{0}; step<1000; ++step)
     {
         std::tie(f1, f2) = models::LGK(V, R, dT, C0, A);
         J = diff::calculateGrads<models::LGK>(V, R, dT, C0, A);
@@ -24,7 +24,7 @@ std::tuple<double, double> LGKSolver(double dT, double C0, const alloy::Alloy& A
         R += 0.1 * dR;
     }
 
-    return std::tuple(V, R);
+    return std::tuple(V, R, std::abs(f1)+std::abs(f2));
 }
 
 // numericallly derived fit from https://doi.org/10.1007/s10854-025-14979-6
@@ -33,15 +33,16 @@ double getPublishedSnAgVFit(double dT, double C0)
     return std::pow(dT*std::pow(C0, -0.59)/28.6, 1/0.35);
 }
 
-TEST_CASE("LGK model V prediction agrees with published numerical fit and gives positive R", "[solver]")
+TEST_CASE("LGK model V prediction agrees with published SnAg numerical fit and gives positive R", "[solver]")
 {
-    double VPred{}, RPred{};
+    double VPred{}, RPred{}, f{};
     for (double dT{10.0}; dT<=60; dT+=10.0)
     {
         for (double C0{3.0}; C0<=6.0; C0+=1.0)
         {
             INFO("dT = " + std::to_string(dT) + ", and C0 = " + std::to_string(C0));
-            std::tie(VPred, RPred) = LGKSolver(dT, C0, alloy::SnAg);
+            std::tie(VPred, RPred, f) = LGKSolver(dT, C0, alloy::SnAg);
+            REQUIRE(f<2e-12);
             REQUIRE(RPred > 0);
 
             double VFit{getPublishedSnAgVFit(dT, C0)};
