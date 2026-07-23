ac97c5f28a55c34286306ee76f65541038e5e95f
ayham <ayham.saffar@gmail.com>
2026-07-23 06:07:25 +0100

    Changed phase diagram fitting script to fit Cs(T) and Cl(T) instead of k0(T)

--- data/Phase_Diagrams/analysis.py
+++ data/Phase_Diagrams/analysis.py
@@ -12,61 +12,46 @@ assert data_path.is_file(), f"Could not find data.csv file in {home_path}"
 
 # %%
 data = pd.read_csv(data_path)
-for col in ["T / K", "Cl", "Cs"]:
+for col in ["T", "Cl", "Cs"]:
     assert col in data.columns, f"Could not find {col} Column in {data.columns.to_list()} columns"
 
 # %%
-orders = range(2, 8)
-fig, axes = plt.subplots(ncols=len(orders), nrows=2, figsize=(6*len(orders),10))
-data['k0'] = data['Cs'] / data['Cl']
-Tl_fits, all_Tl_fit_errors, k0_fits, all_k0_fit_errors = [], [], [], []
+orders = range(3, 8)
+fits = [('Cl', 'T'), ('T', 'Cl'), ('T', 'Cs')] # parameter pairs to fit (x axis, y axis)
+fig, axes = plt.subplots(ncols=len(orders), nrows=len(fits), figsize=(6*len(orders), 5*len(fits)))
+stat_data = []
 
 for i, order in enumerate(orders):
     axes[0, i].set_title(f'{order} order Fits')
 
-    # plotting Cl to Tl fit
-    axes[0, i].set_xlabel("$ C_L $")
-    axes[0, i].set_ylabel("T / K")
-    axes[0, i].scatter(data['Cl'], data['T / K'], color='black', marker='x')
-    Tl_fit = np.polynomial.Polynomial.fit(data['Cl'], data['T / K'], deg=order)
-    axes[0, i].plot(np.arange(1, 101), Tl_fit(np.arange(1, 101)), color='red', linestyle='--')
-    twin_ax = axes[0, i].twinx()
-    twin_ax.set_ylabel('error')
-    T_errors = np.abs(data['T / K']-Tl_fit(data['Cl']))
-    twin_ax.bar(data['Cl'], T_errors, color='grey', alpha=0.2)
-    Tl_fits.append(Tl_fit)
-    all_Tl_fit_errors.append(T_errors)
+    for j, (x, y) in enumerate(fits):
+        ax = axes[j, i]
 
-    # plotting T to k0 fit
-    axes[1, i].set_xlabel("T / K")
-    axes[1, i].set_ylabel("k0")
-    axes[1, i].set_ylim(0, 1)
-    axes[1, i].scatter(data['T / K'], data['k0'], color='black', marker='x')
-    k0_fit = np.polynomial.Polynomial.fit(data['T / K'], data['k0'], deg=order)
-    fit_T, fit_k0 = k0_fit.linspace()
-    axes[1, i].plot(fit_T, fit_k0,  color='red', linestyle='--')
-    twin_ax = axes[1, i].twinx()
-    twin_ax.set_ylabel('error')
-    k0_errors = np.abs(data['k0']-k0_fit(data['T / K']))
-    twin_ax.bar(data['T / K'], k0_errors, color='grey', alpha=0.5)
-    k0_fits.append(k0_fit)
-    all_k0_fit_errors.append(k0_errors)
+        ax.set_xlabel(x)
+        ax.set_ylabel(y)
+        ax.scatter(data[x], data[y], color='black', marker='x')
+
+        fit = np.polynomial.Polynomial.fit(data[x], data[y], deg=order)
+        y_fit = fit(data[x])
+        ax.plot(data[x], y_fit, color='red', linestyle='--')
+        ax = ax.twinx()
+        ax.set_ylabel(f'Error')
+        y_errors = np.abs(data[y]-y_fit)
+        ax.bar(data[x], y_errors, color='grey', alpha=0.5)
+
+        stat_row = dict()
+        stat_row['kind'] = f'{x} to {y}'
+        stat_row['order'] = order
+        coefs = fit.convert().coef
+        for coef in range(max(orders)+1):
+            stat_row[f'{coef} order coefficient'] = coefs[coef] if coef<len(coefs) else 0
+        stat_row['mean error'] = np.mean(y_errors)
+        stat_row['max error'] = np.max(y_errors)
+        stat_data.append(stat_row)
+
 
 fig.tight_layout()
 fig.savefig(home_path / "fits.png")
-
-# %%
-all_fit_stats = []
-for fits, error_arrays in ((Tl_fits, all_Tl_fit_errors), (k0_fits, all_k0_fit_errors)):
-    for fit, error_array in zip(fits, error_arrays):
-        fit_stats = {"kind": "Cl to Tl" if fits==Tl_fits else "T to k0"}
-        coefs = fit.convert().coef
-        fit_stats['order'] = len(coefs) - 1 # -1 as 0th order coefficient exists 
-        for order in range(max(orders)+1):
-            fit_stats[f'{order} order coefficient'] = coefs[order] if order<len(coefs) else 0
-        fit_stats['mean error'] = np.mean(error_array)
-        fit_stats['max error'] = np.max(error_array)
-        all_fit_stats.append(fit_stats)
-
-pd.DataFrame(all_fit_stats).to_csv(home_path / "fits.csv", index=False)
+df = pd.DataFrame(stat_data).sort_values(by=['kind', 'order'])
+df.to_csv(home_path / "fits.csv", index=False)
 
