7c19bce25c824281439ace5b2f69a0f2b1836bd0
ayham <ayham.saffar@gmail.com>
2026-07-25 00:39:04 +0100

    Added temp fix for Enzyme bug which would cause incorrect grads of the CLW model

--- include/differentials.h
+++ include/differentials.h
@@ -17,29 +17,31 @@ namespace diff
         std::tuple<double, double, models::DTs> f{func(V, R, dT, C0, A)};
         return std::get<fToReturn-1>(f);
     }
-    struct Jacobian{double df1dV{}; double df1dR{}; double df2dV{}; double df2dR{};};
     
+    struct Jacobian{double df1dV{}, df1dR{}, df2dV{}, df2dR{};};
+    
+    struct Diffs{double grad1{}, grad2{};};
+
+    //! As of 25/07/26, Enzyme has a bug where it can sometimes modify arguements passed to it. This means subsequent
+    //! calls to __enyme_autodiff evalaute the gradient at the wrong point, giving bad results without any errors being
+    //! raised. At the point of writing this, this only occurs when the CLW model is used, where A's m_TlAtCFit private
+    //! member variable gets modified. I assume this happens because the CLW model is currently the only model that
+    //! calls a member function of A. That is why A is passed by copy to the below function. If passed by const
+    //! reference, the modified A will persist and silently cause issues.
     template <models::ModelFunc modelFunc>
-    inline Jacobian calculateGrads(double V, double R, double dT, double C0, const alloys::Alloy& A)
+    inline Jacobian calculateGrads(double V, double R, double dT, double C0, alloys::Alloy A)
     {
-        Jacobian J{};
-        J.df1dV = __enzyme_autodiff<double>(
+        auto [df1dV, df1dR] = __enzyme_autodiff<Diffs>(
             (void*)wrapper<modelFunc, 1>,
-            enzyme_out, V, enzyme_const, R, enzyme_const, dT, enzyme_const, C0, enzyme_const, &A
+            enzyme_out, V, enzyme_out, R, enzyme_const, dT, enzyme_const, C0, enzyme_const, &A
         );
-        J.df1dR = __enzyme_autodiff<double>(
-            (void*)wrapper<modelFunc, 1>,
-            enzyme_const, V, enzyme_out, R, enzyme_const, dT, enzyme_const, C0, enzyme_const, &A
-        );
-        J.df2dV = __enzyme_autodiff<double>(
+        //! point where A gets modified by CLW model. Luckily the member of A that gets modified is not used to 
+        //! calculate any of the f2 grads, 
+        auto [df2dV, df2dR] = __enzyme_autodiff<Diffs>(
             (void*)wrapper<modelFunc, 2>,
-            enzyme_out, V, enzyme_const, R, enzyme_const, dT, enzyme_const, C0, enzyme_const, &A
+            enzyme_out, V, enzyme_out, R, enzyme_const, dT, enzyme_const, C0, enzyme_const, &A
         );
-        J.df2dR = __enzyme_autodiff<double>(
-            (void*)wrapper<modelFunc, 2>,
-            enzyme_const, V, enzyme_out, R, enzyme_const, dT, enzyme_const, C0, enzyme_const, &A
-        );
-        return J;
+        return Jacobian{df1dV, df1dR, df2dV, df2dR};
     }
 }
 
