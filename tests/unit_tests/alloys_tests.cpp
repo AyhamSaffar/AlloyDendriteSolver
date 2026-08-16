9da0274d9a7e1a933b52017bfcefff43122679f6
ayham <ayham.saffar@gmail.com>
2026-08-17 00:33:29 +0100

    added basic unit tests for Alloy phase diagram methods

---
+++ tests/unit_tests/alloys_tests.cpp
@@ -0,0 +1,19 @@
+#include <catch2/catch_test_macros.hpp>
+#include <vector>
+#include "alloys.h"
+
+TEST_CASE("Alloy phase diagram methods give correct results for linear phase diagram", "[alloys]")
+{
+    double Tm{500}, ml{-10}, ms{-20}; // melting temperature, liquidus gradient, and solidus gradient
+    // fits given as coefficients of polynomials. 1st num is y intercept and second num is gradient
+    std::vector<double> TlAtC{Tm, ml}, ClAtT{-Tm/ml, 1/ml}, CsAtT{-Tm/ms, 1/ms};
+    alloys::Alloy A{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, TlAtC, ClAtT, CsAtT, 1};
+
+    double C{10}, T{50};
+    REQUIRE(A.TlAtC(C) == (Tm+ml*C));
+    REQUIRE(A.mlAtC(C) == ml);
+    REQUIRE(A.ClAtT(T) == (T-Tm)/ml);
+    REQUIRE(A.CsAtT(T) == (T-Tm)/ms);
+    REQUIRE(A.mlAtT(T) == ml);
+    REQUIRE(A.msAtT(T) == ms);
+}
